4c403681a9e90e4e92c59d0456c2f6a4d98dddc1
Modar Yaghi <mfyaghi81@gmail.com>
2023-06-16 04:00:43 +0300

    updates

--- monty_hall.py
+++ monty_hall.py
@@ -1,14 +1,17 @@
 import random
+import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from scipy import stats
+
+sns.set(style='darkgrid')
 
 
 def monty_hall_sim(num_trials=1000):
     switch_wins = 0
     stay_wins = 0
-    switch_win_prob = []
-    stay_win_prob = []
+    switch_win_proba = []
+    stay_win_proba = []
 
     for _ in range(num_trials):
         doors = [0, 0, 1]
@@ -22,10 +25,10 @@ def monty_hall_sim(num_trials=1000):
         if doors[switch_choice] == 1:
             switch_wins += 1
 
-        switch_win_prob.append(switch_wins / (switch_wins + stay_wins))
-        stay_win_prob.append(stay_wins / (switch_wins + stay_wins))
+        switch_win_proba.append(switch_wins / (switch_wins + stay_wins))
+        stay_win_proba.append(stay_wins / (switch_wins + stay_wins))
 
-    return switch_win_prob, stay_win_prob
+    return switch_win_proba, stay_win_proba
 
 
 switch_win_prob, stay_win_prob = monty_hall_sim(10000)
@@ -36,19 +39,19 @@ ax.set_ylabel('Winning Probability')
 ax.set_title('Monty Hall Problem Simulation')
 ax.grid(True)
 ax.set_ylim([0, 1])
-ax.plot([], [], 'r-', label='Switch')
-ax.plot([], [], 'b-', label='Stay')
-ax.axhline(y=2/3, color='r', linestyle='--')
-ax.axhline(y=1/3, color='b', linestyle='--')
+ax.plot([], [], 'g-', label='Switch')
+ax.plot([], [], 'r-', label='Stay')
+ax.axhline(y=2 / 3, color='g', linestyle='--')
+ax.axhline(y=1 / 3, color='r', linestyle='--')
 ax.legend()
 
 
 def update(num):
     ax.clear()
-    ax.plot(switch_win_prob[:num], 'r-', label='Switch')
-    ax.plot(stay_win_prob[:num], 'b-', label='Stay')
-    ax.axhline(y=2/3, color='r', linestyle='--')
-    ax.axhline(y=1/3, color='b', linestyle='--')
+    ax.plot(switch_win_prob[:num], 'g-', label='Switch')
+    ax.plot(stay_win_prob[:num], 'r-', label='Stay')
+    ax.axhline(y=2 / 3, color='g', linestyle='--')
+    ax.axhline(y=1 / 3, color='r', linestyle='--')
     ax.legend()
     ax.set_xlabel('Trials')
     ax.set_ylabel('Winning Probability')
@@ -59,3 +62,7 @@ def update(num):
 
 ani = FuncAnimation(fig, update, frames=len(switch_win_prob), repeat=False)
 plt.show()
+
+# Perform a statistical test
+t_stat, p_val = stats.ttest_ind(switch_win_prob, stay_win_prob)
+print(f"t-statistic: {t_stat}, p-value: {p_val}")
